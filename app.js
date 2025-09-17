f607fac23b5adf1bfc5b3657bfe4512e3c38d75b
Radovan Trnka <rodaq1704@gmail.com>
2025-09-17 21:15:29 +0200

    feat: Initial web-app (calculator)

---
+++ app.js
@@ -0,0 +1,59 @@
+let currentInput = '';
+let currentOperation = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    document.getElementById('result-field').value = `${previousInput} ${currentOperation} ${currentInput}`;
+}
+
+function appendOperation(operation) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate(); 
+    }
+    currentOperation = operation;
+    previousInput = currentInput;
+    currentInput = '';
+    document.getElementById('result-field').value = `${previousInput} ${currentOperation}`;
+}
+
+function calculate() {
+    if (previousInput === '' || currentInput === '') return;
+    let result;
+    let prev = parseFloat(previousInput);
+    let current = parseFloat(currentInput);
+
+    switch (currentOperation) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            if (current === 0) {
+                alert("Delit nulou sa neda");
+                return;
+            }
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    currentInput = result.toString();
+    currentOperation = '';
+    previousInput = '';
+    document.getElementById('result-field').value = currentInput;
+}
+
+function clearResultField() {
+    currentInput = '';
+    previousInput = '';
+    currentOperation = '';
+    document.getElementById('result-field').value = '';
+}
